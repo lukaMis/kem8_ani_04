541ce9b2cec19958391adde933026867bf7824a2
Luka Mis <luka.mis@gmail.com>
2017-02-02 16:04:34 +0100

    Animation 03 we have svg in the dom. Time to start animating…

--- anim/anim_03/assets/js/src/Bootstrap.js
+++ anim/anim_03/assets/js/src/Bootstrap.js
@@ -5,9 +5,20 @@ kem8_ani_04_anim_03.Bootstrap = () => {
   'use strict';
   const instance = {};
 
-  
+  const svgLoader = kem8_ani_04_anim_03.SvgLoader();
+  $(svgLoader).one('svgLoaded', () => {
+    console.log('svg loaded');
+    // controller.init();
+
+    // parent iframe removes preloader on this event
+    $(document).trigger('onContentLoaded');
+  });
+
+  const init = () => {
+    svgLoader.load();
+  };
+  init();  
   /* API */
-  
 
   console.log('Bootstrap works');
   return instance;
--- anim/anim_03/assets/js/src/SvgLoader.js
+++ anim/anim_03/assets/js/src/SvgLoader.js
@@ -5,8 +5,25 @@ kem8_ani_04_anim_03.SvgLoader = () => {
   'use strict';
   const instance = {};
 
+
+  const load = () => {
+    Snap.load('assets/images/anim_03.svg', (f) => {
+      console.log('snap has loaded svg fragment');
+
+      // add svg to dom
+      Snap('#svg-wrapper').append(f);
+
+      // internal event for this animation
+      $(instance).trigger('svgLoaded');
+    });
+  };
+
   
   /* API */
+  instance.init = () => {};
+  instance.load = () => {
+    load();
+  };
   
 
   console.log('SvgLoader works');
