3b7f0bb6ddcac96475d5a0036cd7af32eb62d001
Luka Mis <luka.mis@gmail.com>
2017-02-03 12:26:17 +0100

    Working on animation 03….

--- anim/anim_03/assets/js/src/Controller.js
+++ anim/anim_03/assets/js/src/Controller.js
@@ -10,10 +10,20 @@ kem8_ani_04_anim_03.Controller = () => {
 
   let fireArray = [];
 
+  let rightCircleArray = [];
+
+  // for transfrom value :)
+  // console.log(this.target[0]._gsTransform.x );
+
 
   const getDomReferences = () => {
     fireArray[0] = $('#fire-0');
     fireArray[1] = $('#fire-1');
+
+    rightCircleArray[0] = { target: $('#circle-1-molecule-0'), moveSpeedX: 1, moveSpeedY: 1 };
+    TweenMax.set(rightCircleArray[0].target, {
+      transformOrigin: '50% 50%'
+    });
   };
 
 
@@ -21,6 +31,7 @@ kem8_ani_04_anim_03.Controller = () => {
     setInitialStateForFire();
     fireAimation();
 
+    rightCircleAnimation();
   };
 
 
@@ -61,6 +72,68 @@ kem8_ani_04_anim_03.Controller = () => {
       scaleY : getRandomNumber(0.65, 1.2),
       onComplete: fireAimation
     });
+  };
+
+
+  const rightCircleAnimation = () => {
+
+
+    TweenMax.to(rightCircleArray[0].target, 1, {
+      ease: Power0.easeNone,
+      // x: rightCircleArray[0].transformX,
+      // y: rightCircleArray[0].transformY,
+      // x: getPointInCircle().x,
+      // y: getPointInCircle().y,
+      rotation: '+=1',
+      onComplete: function() {
+        rightCircleAnimation();
+      },
+      onStart: function() {
+        // getDirection(this.target[0]._gsTransform, rightCircleArray[0]);
+        // getPointInCircle();
+      }
+    });
+  };
+
+  const getPointInCircle = () => {
+    // let angle = getRandomNumber(0, 2 * Math.PI);
+    // let newX = Math.floor(110 - 33 * Math.cos(angle));
+    // let newY = Math.floor(110 - 33 Math.sin(angle));
+    console.log(newX, newY);
+    // return {
+    //   x: newX,
+    //   y: newY
+    // }
+  };
+
+  const getDirection = (currentTransform, atom) => {
+    if(currentTransform.x > 78 && atom.moveSpeedX === 1) {
+      atom.moveSpeedX = atom.moveSpeedX * -1;
+    }
+    else if(currentTransform.x < -78 && atom.moveSpeedX === -1) {
+      atom.moveSpeedX = atom.moveSpeedX * -1;
+    }
+    // console.log( getRandomNumber(0.5, 1.5).toFixed(2) );
+    let returnDirectionX = (atom.moveSpeedX > 0) ? '+=':'-=';
+    returnDirectionX += Math.floor(getRandomNumber(1, 3));
+    // console.log(currentTransform.x, atom.moveSpeedX, returnDirectionX);
+    atom.transformX = returnDirectionX;
+
+
+    // console.log('currentTransform.y', currentTransform.y);
+    if(currentTransform.y > 78 && atom.moveSpeedY === 1) {
+      atom.moveSpeedY = atom.moveSpeedY * -1;
+      atom.moveSpeedX = atom.moveSpeedX * -1;
+    }
+    else if(currentTransform.y < -78 && atom.moveSpeedY === -1) {
+      atom.moveSpeedY = atom.moveSpeedY * -1;
+      atom.moveSpeedX = atom.moveSpeedX * -1;
+    }
+    let returnDirectionY = (atom.moveSpeedY > 0) ? '+=':'-=';
+    // console.log(currentTransform.y, atom.moveSpeedY, returnDirectionY);
+    returnDirectionY += Math.floor(getRandomNumber(1, 3));
+    atom.transformY = returnDirectionY;
+    // return returnDirection;
   };
 
 
